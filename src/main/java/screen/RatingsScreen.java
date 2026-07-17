f97eecbda720a7a0668a865d6993e30aad7a240c
kushal-koochana <koochanakushal@gmail.com>
2026-07-17 10:14:35 +0100

    Refactor RatingsScreen layout and remove comments

--- src/main/java/screen/RatingsScreen.java
+++ src/main/java/screen/RatingsScreen.java
@@ -28,25 +28,21 @@ public class RatingsScreen {
         panel.setLayout(new GridBagLayout());
 
         GridBagConstraints constraints = new GridBagConstraints();
-        
-        // Fill space, and use non-zero weights so that fill works
+
         constraints.weightx = 2.0;
         constraints.weighty = 1.0;
         constraints.fill = GridBagConstraints.BOTH;
-        Insets insets = new Insets(5,5,5,5); //Add space around reels
+        Insets insets = new Insets(5,5,5,5);
         constraints.insets = insets;
 
-        // Make the reels 3 grid units wide, in the leftmost position
         int reelsWidth = 1;
         constraints.gridwidth = reelsWidth;
         constraints.gridx = 0;
         constraints.gridy = 0;
 
-        // The reels that will be scrolled
         JPanel reels = new JPanel();
         reels.setBorder(BorderFactory.createEmptyBorder());
-        
-        // Make layout vertically stacked
+
         reels.setLayout(new BoxLayout(reels, BoxLayout.Y_AXIS));
         
         MovieReel mostRatedMovies = new MostRatedMovieReel(panel, stores);
@@ -66,22 +62,17 @@ public class RatingsScreen {
 
         reels.setSize(new Dimension(reelsScrollPane.getWidth(), reelsScrollPane.getHeight()));
 
-        // Restrict the reels pane (which has a vertical stack of reels) to be the width of the pane scrolling it
-        // This stops the reels scrollpane within it from just being really wide and not having a scroll bar,
-        // where the reelsScrollPane just started scrolling everything
         reels.setPreferredSize(new Dimension(reelsScrollPane.getWidth(), reelsScrollPane.getHeight()));
 
         panel.add(reelsScrollPane, constraints);
 
-
-        // Get the top 100 users with the most ratings & display their UID, average rating and number of ratings. Ordered by number of reviews, descending.
         TitledBorder mostRatingsUserBorder;
         mostRatingsUserBorder = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Constants.getFontColor()), "Most Ratings from Users");
         mostRatingsUserBorder.setTitleJustification(TitledBorder.CENTER);
         mostRatingsUserBorder.setTitleColor(Constants.getFontColor());
 
 
-        JPanel mostRatingsUserInner = new JPanel(); // The panel that is getting scrolled around
+        JPanel mostRatingsUserInner = new JPanel();
         mostRatingsUserInner.setBackground(Constants.getHighlight());
         mostRatingsUserInner.setForeground(Constants.getFontColor());
         JScrollPane mostRatingsUserScrollPane = new JScrollPane(mostRatingsUserInner);
@@ -93,13 +84,10 @@ public class RatingsScreen {
         MostRatedUsersRunnable mostRatedUsersRunnable = new MostRatedUsersRunnable(panel, mostRatingsUserScrollPane, mostRatingsUserInner, ratings);
 
 
-        constraints.insets = new Insets(5,5,5,5); // Add spacing around the panel
-        // Make the most rated users list one 'grid cell' wide, and in the location after the 3-wide reels
+        constraints.insets = new Insets(5,5,5,5);
         constraints.weightx = 0.2;
         constraints.gridwidth = 1;
-        constraints.gridx = 1; // place this in the grid cell to the right of the reels
-
-
+        constraints.gridx = 1;
 
         panel.add(mostRatingsUserScrollPane, constraints);
 

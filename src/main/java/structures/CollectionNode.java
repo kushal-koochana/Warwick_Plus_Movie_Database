7c17217006427791889b1fcdaa854b97fef12c11
kushal-koochana <koochanakushal@gmail.com>
2026-07-17 10:25:36 +0100

    Remove constructor documentation in CollectionNode
    Removed unnecessary constructor documentation for CollectionNode.

--- src/main/java/structures/CollectionNode.java
+++ src/main/java/structures/CollectionNode.java
@@ -8,16 +8,6 @@ public class CollectionNode {
 
     public MyDynamicArray<Integer> filmIDs = new MyDynamicArray<>();
 
-
-    /**
-     * Constructs a new CollectionNode with the supplied metadata. The filmIDS array is initially empty; films are added
-     * separately via Movies.addToCollection
-     *
-     * @param id             The unique TMDB collection ID
-     * @param name           The display name of the collection
-     * @param posterPath     The URL path segment for the collection poster
-     * @param backdropPath   The URL path segment for the collection backdrop
-     */
     public CollectionNode(int id, String name, String posterPath, String backdropPath) {
         this.id = id;
         this.name = name;
